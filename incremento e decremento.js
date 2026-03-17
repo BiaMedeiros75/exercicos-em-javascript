f3f0c7419c838ff5ad06f60ce2a68c9ac37ca0a4
Anna Medeiros <122562181+BiaMedeiros75@users.noreply.github.com>
2026-03-16 23:32:36 -0300

    Add files via upload

---
+++ incremento e decremento.js
@@ -0,0 +1,23 @@
+let counter = 10;
+console.log(counter);
+
+let updatedCounter = ++counter;
+console.log(updatedCounter);
+
+let score = 8;
+console.log(score);
+
+let finalScore = score++;
+console.log(finalScore);
+
+let coins = 3;
+console.log(coins);
+
+let updatedCoins = --coins;
+console.log(updatedCoins);
+
+let health = 7;
+console.log(health);
+
+let newHealth = health--;
+console.log(newHealth);
---
+++ triviabot.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Bia";
+let botLocation = "Brazil";
+let favoriteLanguage = "Javascript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".")
+
+console.log("My favorite programming language is " + favoriteLanguage + ".")
+
+let codingFact = "It is a easy languege to learn " + favoriteLanguage;
+
+console.log(codingFact);
+
+codingFact = "It is a simple language " + favoriteLanguage;
+
+console.log(codingFact);
+
+codingFact = "It is a beautiful language " + favoriteLanguage;
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
